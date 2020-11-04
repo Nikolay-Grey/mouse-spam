d0dbe73b513260177c21c024e7b3ae0f468477e3
Nikolay Sergeev <59118268+Nikolay-Grey@users.noreply.github.com>
2020-11-04 17:24:28 +0300

    Add files via upload
    Added main.sh and main.py (see README)

---
+++ main.sh
@@ -0,0 +1,11 @@
+python3 -c '
+__author__ = "Nikolay Sergeev"
+from os import system
+from random import randint, choice
+#while 1:
+for i in range(20):
+    b = randint(-1000, 1000)
+    a = randint(-1000, 1000)
+    system("xdotool mousemove_relative -- " + str(b) + " " + str(a))
+    system("xdotool click " + str(choice([0, 1, 2, 3, 4, 5])))
+'
