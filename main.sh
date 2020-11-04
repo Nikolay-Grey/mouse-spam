25b7ed72b8028e38719fd3c56952a4b4d00e1bc1
Nikolay <sergeev.nicolas.57@gmail.com>
2020-11-04 17:47:34 +0300

    Minor changes

--- main.sh
+++ main.sh
@@ -2,8 +2,7 @@ python3 -c '
 __author__ = "Nikolay Sergeev"
 from os import system
 from random import randint, choice
-#while 1:
-for i in range(20):
+while 1:
     b = randint(-1000, 1000)
     a = randint(-1000, 1000)
     system("xdotool mousemove_relative -- " + str(b) + " " + str(a))
