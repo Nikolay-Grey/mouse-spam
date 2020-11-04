1c0cd489a29ce7be64fb980b24029b18a1856c46
Nikolay <sergeev.nicolas.57@gmail.com>
2020-11-04 17:42:58 +0300

    Remove useless files (Uploaded by mistake)

--- lol.py
+++
@@ -1,8 +0,0 @@
-from os import system
-from random import randint
-#while 1:
-for i in range(20):
-    b = randint(-1000, 1000)
-    a = randint(-1000, 1000)
-    system("xdotool mousemove_relative -- " + str(b) + " " + str(a))
-    system("xdotool click 1")
--- lol.sh
+++
@@ -1,8 +0,0 @@
-python3 -c '
-from os import system
-from random import randint
-for i in range(20):
-    b = randint(-1000, 1000)
-    a = randint(-1000, 1000)
-    system("xdotool mousemove_relative -- " + str(b) + " " + str(a))
-    system("xdotool click 1")'
